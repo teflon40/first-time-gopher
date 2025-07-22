2407b3cf7fa1702c7310ccf9fd557f9af73dd3bd
teflon40 <frimpong.bernard3220@gmail.com>
2025-07-22 04:14:54 +0000

    Add implementation to print digits from 0 to 9 as characters

---
+++ 0x01-variables_if_else_while/5-print_numbers.go
@@ -0,0 +1,11 @@
+// Print the digits from 0 to 9 as characters
+package main
+
+import "fmt"
+
+func main() {
+    for i := 48; i <= 57; i++ {
+        fmt.Printf("%c", i)
+    }
+    fmt.Println() // Print a newline at the end
+}
